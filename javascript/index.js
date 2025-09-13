79dc3a617614b12b946a6702ed55320aea9e53fb
gabbybeall <gabriellebeall@gmail.com>
2025-09-13 10:34:17 -0700

    Added JavaScript to include real time and date for 2 cities

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+function updateTime() {
+  let sanFranciscoElement = document.querySelector("#san-francisco");
+  let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+  let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+  let sanFranciscoTime = moment().tz("America/Los_Angeles");
+  sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
+  sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
